5f4bbd5a3e7ac1a4f56073c52f997252611adf08
Amin Al Ali Al Darwish <amin@bunnyshell.com>
2023-02-22 13:48:57 +0400

    update function

--- functions/index.py
+++ functions/index.py
@@ -1,19 +1,19 @@
 import http.client
+import random
+import string
 import json
 import os
 
 def handler(event, context):
-    app_url = os.environ['APP_URL']
-    print(app_url)
-    conn = http.client.HTTPSConnection(app_url)
-    payload = json.dumps({
-        "taskId": "First Things First",
-        "taskDescription": "Define your application"
-    })
+    letters = string.ascii_lowercase
+    title = ''.join(random.choice(letters) for i in range(10))
+    description = ''.join(random.choice(letters) for i in range(20))
+    conn = http.client.HTTPSConnection("backend-tksd7k.bunnyenv.com")
+    payload = 'title='+title+'&description='+description
     headers = {
-      'Content-Type': 'application/json'
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
-    conn.request("POST", "/todos", payload, headers)
+    conn.request("POST", "/books", payload, headers)
     res = conn.getresponse()
     data = res.read()
     print(data.decode("utf-8"))
